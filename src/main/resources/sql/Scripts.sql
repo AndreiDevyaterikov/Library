f49524de1b1960c826756cebd8517b0683270bab
AndreiDevyaterikov <devyaterikov.andrei@gmail.com>
2023-08-13 19:58:08 +0300

    delete r2dbc, added jpa

--- src/main/resources/sql/Scripts.sql
+++ src/main/resources/sql/Scripts.sql
@@ -1,23 +1,23 @@
-create table if not exists author
+create table if not exists authors
 (
     id   integer primary key,
     name varchar not null
 );
 
-create table if not exists book
+create table if not exists books
 (
     id integer primary key,
     title varchar not null,
     count integer not null
 );
 
-create table if not exists book_author
+create table if not exists book_authors
 (
     book_id integer not null,
     author_id integer not null,
     primary key (book_id, author_id),
-    foreign key (book_id) references book(id),
-    foreign key (author_id) references author(id)
+    foreign key (book_id) references books(id),
+    foreign key (author_id) references authors(id)
     );
 
 create table if not exists logbook
@@ -28,9 +28,5 @@ create table if not exists logbook
     issue_date timestamp,
     return_date timestamp,
     status varchar,
-    foreign key (book_id) references book(id)
+    foreign key (book_id) references books(id)
 );
-
-insert into author(id, name)
-values (1, 'test1'),
-       (2, 'test2')
